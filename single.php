33804f3c2afeac8f50a1c2ac41382145dc16c71f
JrmyDlms <173142512+JrmyDlms@users.noreply.github.com>
2025-01-06 16:39:04 +0100

    init

---
+++ single.php
@@ -0,0 +1,26 @@
+<?php
+get_header(); ?>
+
+<main class="site-main">
+    <?php
+    while (have_posts()) :
+        the_post(); ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+                <h1><?php the_title(); ?></h1>
+            </header>
+
+            <?php if (has_post_thumbnail()) : ?>
+                <div class="post-thumbnail">
+                    <?php the_post_thumbnail(); ?>
+                </div>
+            <?php endif; ?>
+
+            <div class="entry-content">
+                <?php the_content(); ?>
+            </div>
+        </article>
+    <?php endwhile; ?>
+</main>
+
+<?php get_footer(); ?>
